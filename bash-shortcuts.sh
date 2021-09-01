a0a2b5b9468ef3ffee2e462ec803ca2b8a589a14
Geoffrey Hoffman <geoffrey.hoffman@advinow.com>
2021-09-01 10:24:56 -0700

    Fixed vpnd

--- bash-shortcuts.sh
+++ bash-shortcuts.sh
@@ -3,14 +3,22 @@
 # Faster bash
 #
 
+
+
 alias lh='ls -lhart'
 
-
 alias neo4j='cd ~/Apps/neo4j-desktop-1.4.7 && ./neo4j-desktop-1.4.7-x86_64.AppImage'
+
+
+# OpenVPN3 Shortcuts
 alias vpnu='openvpn3 session-start --config ~/.openvpn3/advinow.conf'
 alias vpns='openvpn3 sessions-list'
 alias vpnr='openvpn3 session-manage --config ~/.openvpn3/advinow.conf --restart'
-alias vpnd='openvpn3 session-manage --session-path ~/.openvpn3/advinow.conf --disconnect'
+function vpn_down(){
+	DBUS=$(vpns | head -n2 | tail -n1 | awk '{print $2}')
+	openvpn3 session-manage --session-path $DBUS --disconnect
+}
+alias vpnd='vpn_down'
 alias vpnk='sudo pkill openvpn'
 alias pstm='cd ~/Apps/Postman-linux-x86_64-8.11.1/Postman && ./Postman' 
 
